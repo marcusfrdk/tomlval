f45c49d16ccc507cf681bec254a774868b24a2a7
Marcus Fredriksson <marcus@marcusfredriksson.com>
2025-07-24 00:15:36 +0200

    delete: deleted old files in preparation for new structure and files

--- tomlval/types/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Types for the TOML parser."""
-
-from tomlval.types.handler import Handler
-from tomlval.types.path_or_str import PathOrStr
--- tomlval/types/path_or_str.py
+++
@@ -1,6 +0,0 @@
-""" Typing for paths or strings. """
-
-import pathlib
-from typing import Union
-
-PathOrStr = Union[str, pathlib.Path]
